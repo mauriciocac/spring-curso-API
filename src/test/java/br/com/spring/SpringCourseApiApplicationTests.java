9e3bcd80f8f837b17a61d6bcad945ffd56b4c4ca
Mauricio Carvalho <mauricio.cac@hotmail.com>
2020-10-13 21:34:45 -0300

    Primeiro commit do projeto

---
+++ src/test/java/br/com/spring/SpringCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
