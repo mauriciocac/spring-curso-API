805bd43a9c85d6e3a3bf39b2a61c8824c294a08f
Mauricio Carvalho <mauricio.cac@hotmail.com>
2020-10-14 22:17:19 -0300

    ajustando o repository do projeto

--- src/main/java/br/com/spring/repository/RequestRepository.java
+++ src/main/java/br/com/spring/repository/RequestRepository.java
@@ -1,5 +1,19 @@
 package br.com.spring.repository;
 
-public interface RequestRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.spring.domain.Request;
+import br.com.spring.domain.enums.RequestState;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long>{
+	
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("update Request set state = ?2 where id =?1")
+	public Request updateStatus(Long id, RequestState state);
 }
